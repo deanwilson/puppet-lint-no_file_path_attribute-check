3269e9de46e33ed09372c439da245d689d54f3b2
Dean Wilson <dean.wilson@gmail.com>
2020-07-09 08:35:44 +0100

    Fix linter issues

--- lib/puppet-lint/plugins/no_file_path_attribute.rb
+++ lib/puppet-lint/plugins/no_file_path_attribute.rb
@@ -2,21 +2,20 @@ PuppetLint.new_check(:no_file_path_attribute) do
 
   def check
     resource_indexes.each do |resource|
-      if resource[:type].value == 'file'
-        resource[:param_tokens].select { |param_token|
-          param_token.value == 'path'
-        }.each do |param_token|
-          value_token = param_token.next_code_token.next_code_token
+      next unless resource[:type].value == 'file'
 
-          notify :warning, {
-            message: 'file resources should not have a path attribute. Use the title instead',
-            line:    value_token.line,
-            column:  value_token.column,
-            token:   value_token
-          }
-        end
+      param_tokens = resource[:param_tokens].select { |pt| pt.value == 'path' }
+
+      param_tokens.each do |param_token|
+        value_token = param_token.next_code_token.next_code_token
+
+        notify :warning, {
+          message: 'file resources should not have a path attribute. Use the title instead',
+          line: value_token.line,
+          column: value_token.column,
+          token: value_token,
+        }
       end
     end
   end
-
 end
